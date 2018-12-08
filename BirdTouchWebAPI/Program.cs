9ad96b1d99dabe04efdd1cc1119e8ee4366005c8
Nenad Ilic <ilic1993@gmail.com>
2018-12-08 13:21:50 +0100

    Logging feature test

--- BirdTouchWebAPI/Program.cs
+++ BirdTouchWebAPI/Program.cs
@@ -19,6 +19,13 @@ namespace BirdTouchWebAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                })
                 .UseStartup<Startup>();
     }
 }
--- BirdTouchWebAPI/ScheduledTasks/RemoveInactiveUsersScheduledTask.cs
+++ BirdTouchWebAPI/ScheduledTasks/RemoveInactiveUsersScheduledTask.cs
@@ -27,16 +27,10 @@ namespace BirdTouchWebAPI.ScheduledTasks
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInformation("RemoveInactiveUsersScheduledTask is starting.");
+            _logger.LogInformation($"RemoveInactiveUsersScheduledTask is starting. {DateTime.UtcNow}");
             _logger.LogInformation($"Removing user that have been inactive for " +
                                    $"{_configuration["RemoveInactiveUsersRemoveUsersOlderThan"]} hours.");
             _logger.LogInformation($"Task will run every {_configuration["RemoveInactiveUsersRunEvery"]} minutes.");
-
-            _logger.LogWarning("test1");
-            _logger.LogCritical("test1");
-            _logger.LogDebug("test1");
-            _logger.LogError("test1");
-            _logger.LogTrace("test1");
 
             _timer = new Timer(RemoveInactiveUsers,
                                null,
@@ -48,12 +42,12 @@ namespace BirdTouchWebAPI.ScheduledTasks
 
         private void RemoveInactiveUsers(object state)
         {
-            _logger.LogInformation("RemoveInactiveUsersScheduledTask is working.");
+            _logger.LogInformation($"RemoveInactiveUsersScheduledTask is working. {DateTime.UtcNow}");
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _logger.LogInformation("RemoveInactiveUsersScheduledTask is stopping.");
+            _logger.LogInformation($"RemoveInactiveUsersScheduledTask is stopping. {DateTime.UtcNow}");
 
             _timer?.Change(Timeout.Infinite, 0);
 
